68f1b06935f3d8942bf17501c98401e7a17962fb
rahul071845 <machrarahul25@gmail.com>
2025-07-03 23:12:35 +0530

    debugging

--- backend/index.js
+++ backend/index.js
@@ -36,6 +36,9 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.set("trust proxy", 1);
+
 app.use(session(sessionOptions));
 app.use(passport.initialize());
 app.use(passport.session());
